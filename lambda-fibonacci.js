fb1346f9bf38b952625b0c8c0e14e32a2f035382
Oscar Saharoy <osaharoy@gmail.com>
2021-03-13 18:17:01 +0000

    can create a list of fibonacci numbers up to n and print it

--- lambda-fibonacci.js
+++ lambda-fibonacci.js
@@ -2,12 +2,14 @@
 // function that prints true for K and false for KI
 printBool  = x => console.log( x(true)(false) )
 
-// function that prints the value of a church numeral
-printChurchNumeral = x => console.log( x(y => ++y)(0) )
-
 // function to print pairs of church numerals
 printPair = p => console.log( `(${fst(p)(y => ++y)(0)}, ${snd(p)(y => ++y)(0)})` )
 
+// function that prints the value of a church numeral
+printChurchNumeral = x => process.stdout.write( x(y => ++y)(0) )
+
+// function that prints a list of church numerals
+printList = l => { printChurchNumeral( fst(l) ); if( snd(l) != K ) printList( snd(l) ); }
 
 // bird functions
 I  = x => x                 // ibis
@@ -16,7 +18,6 @@ C  = f => x => y => f(y)(x) // cardinal
 B  = f => g => x => f(g(x)) // bluebird
 TH = f => g => g(f)         // thrush
 BL = B(B)(B)                // blackbird
-
 
 // K and KI are functions representing true and false respectively
 K  = x => y => x
@@ -51,7 +52,10 @@ sub   = n1 => n2 => n2(pred)(n1)
 mul   = n1 => n2 => B(n1)(n2)    // == B
 pow   = n1 => n2 => n2(n1)       // == TH
 
+four  = succ(three)
 five  = add(two)(three)
+six   = mul(two)(three)
+seven = add(two)(five)
 ten   = mul(five)(two)
 
 // number comparisons
@@ -60,10 +64,11 @@ leq   = n1 => n2 => is0( sub(n2)(n1) )
 eq    = n1 => n2 => and( leq(n1)(n2) )( leq(n2)(n1) )
 gt    = BL(not)(leq)
 
-// fibonacci combinator
+
+lK    = () => K // lambda returning K for lazy evaluation
+
+// fibonacci combinators
 fib   = p => P( snd(p) )( add(fst(p))(snd(p)) )
+nFib  = n => p => () => P( fst(p) )( ( gt(n)(fst(p))( lK )( nFib(n)(fib(p)) ) )() )
 
-// push to a stack
-push = x => s => P(s)(x)
-
-printChurchNumeral( snd( ten(fib)( P(one)(one) ) ) )
+printList( nFib( seven )( P(one)(one) )() )
